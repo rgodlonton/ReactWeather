8ec845860913a57e11594aa8d7f0084e8e9c28f0
rgodlonton <ross@soffex.co.za>
2017-02-26 01:09:06 +0200

    Added copy

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,10 +1,8 @@
 var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
-
     return (
-        <h3>It is {temp}
-            in {location}.</h3>
+        <h3>It is {temp} in {location}.</h3>
     );
 }
 
