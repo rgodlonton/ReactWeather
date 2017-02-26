09f8e55b4a504eede0ece45f5e71bb6440ef9f6f
rgodlonton <ross@soffex.co.za>
2017-02-26 03:03:25 +0200

    Style example and about page and add links to resources

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>This is the examples page.</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li><Link to='/?location=Cape Town'>Cape Town, ZA</Link></li>
+                <li><Link to='/?location=Johannesburg'>Johannesburg, ZA</Link></li>
+            </ol>
         </div>
     );
 }
