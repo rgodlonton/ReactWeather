2257a1537115df348b6a53dec8a2b76bea8b75c3
rgodlonton <ross@soffex.co.za>
2017-02-26 02:20:11 +0200

    Introduced foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,13 +6,15 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
-  <Router history={hashHistory}>
+    <Router history={hashHistory}>
     <Route path="/" component={Main}>
         <Route path="about" component={About}/>
         <Route path="examples" component={Examples}/>
         <IndexRoute component={Weather}/>
     </Route>
-  </Router>,
-  document.getElementById('app')
-);
+</Router>, document.getElementById('app'));
