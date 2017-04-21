b700021ce1e47420fcceda6c1fd2e3dde4668a7d
rgodlonton <ross@soffex.co.za>
2017-04-21 22:58:13 +0200

    Set title on About page

---
+++ Examples.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var {Link} = require('react-router')
+
+var Examples = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li><Link to='/?location=Cape Town'>Cape Town, ZA</Link></li>
+                <li><Link to='/?location=Johannesburg'>Johannesburg, ZA</Link></li>
+            </ol>
+        </div>
+    );
+}
+
+module.exports = Examples;
